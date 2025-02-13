752ba262045cfcdf7e57a7bb113216bde1b9aab8
Rimsha Arshad <adeelarshad8900@gmail.com>
2025-02-14 01:14:27 +0500

    hello

--- e_commerce/src/app/product/[slug]/page.tsx
+++ e_commerce/src/app/product/[slug]/page.tsx
@@ -91,14 +91,14 @@ const ProductPage = () => {
     <div className="container mx-auto mt-36">
       <div className="w-full flex gap-8 p-4">
       {product?.image && (
-          <Image
-            src={urlFor(product.image).url()}
-            alt={product?.name || "Product Image"}
-            width={200}
-            height={200}
-            className="rounded-lg w-[30rem] h-[32rem] object-cover"
-          />
-      )}
+  <Image
+    src={urlFor(product.image).url()}
+    alt={product?.name || "Product Image"}
+    width={200}
+    height={200}
+    className="rounded-lg w-[30rem] h-[32rem] object-cover"
+  />
+)}
         <div>
           <h2 className="integral text-[2.3rem] font-semibold">{product?.name}</h2>
           {renderStars(product?.rating)}
